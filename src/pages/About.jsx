424b210cf8bda550110e42641c47fa38164e5e32
Prahith <babluprahith@gmmail.com>
2026-02-26 14:53:04 +0530

    feat: About Page Added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,28 @@
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+import AboutHero from '@/components/sections/AboutHero'
+import CtaSection from '@/components/sections/CtaSection'
+import OperateStats from '@/components/sections/OperateStats'
+import Team from '@/components/sections/Team'
+import Values from '@/components/sections/Values'
+import WorkflowMap from '@/components/sections/WorkflowMap'
 import React from 'react'
 
 const About = () => {
   return (
-    <div className='container-custom'>
-      
-    </div>
+    <main>
+      <section className='container-custom'><Header/></section>
+      <AboutHero/>
+      <section className='bg-neutral-800'><Team/></section>
+      <OperateStats/>
+      <WorkflowMap/>
+      <Values/>
+      <CtaSection heading="Join the Shift Toward"
+        highlight="Intelligent Ops"
+        description="Whether you're scaling across geographies or just starting your AI journey, Zapverse helps you move faster, with less guesswork."
+        buttonText="Talk to Our Team"/>
+      <Footer/>
+    </main>
   )
 }
 
