935711074abc746a4777fdd826b4fe73d1c37768
Prahith <babluprahith@gmmail.com>
2026-02-27 19:54:08 +0530

    updaate: minor code change

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,10 +1,15 @@
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
 import React from 'react'
+import Contact from '@/components/sections/Contact'
 
 const ContactUs = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
+    <main>
+      <section className='container-custom'><Header /></section>
+      <Contact />
+      <Footer />
+    </main>
   )
 }
 
