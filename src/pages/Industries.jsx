a00f8f22ddfc88e9849dddc36d5a469a16455d2b
Prahith <babluprahith@gmmail.com>
2026-02-27 19:53:08 +0530

    feat: Developed New Page

--- src/pages/Industries.jsx
+++ src/pages/Industries.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import CtaSection from '@/components/sections/CtaSection'
+import HeroIndustry from '@/components/sections/HeroIndustry'
+import InfiniteCarousel from '@/components/ui/InfiniteCarousel'
+import Content from '@/components/sections/Content'
+import MordernAI from '@/components/sections/MordernAI'
+import SmartAutomation from '@/components/sections/SmartAutomation'
+import Localization from '@/components/sections/Localization'
 
 const Industries = () => {
   return (
-    <div>
-      <h1>Industries</h1>
-    </div>
+    <main>
+      <section className='container-custom'><Header/></section>
+      <HeroIndustry/>
+      <InfiniteCarousel/>
+      <Content/>
+      <section className='bg-[#191919]'><MordernAI/></section>
+      <SmartAutomation/>
+      <Localization/>
+      <CtaSection heading="Join the Shift Toward"
+        highlight="Intelligent Ops"
+        description="Whether you're scaling across geographies or just starting your AI journey, Zapverse helps you move faster, with less guesswork."
+        buttonText="Talk to Our Team"/>
+      <Footer/>
+    </main>
   )
 }
 
