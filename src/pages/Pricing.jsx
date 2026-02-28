5a848a788663379888c3130cbd4def9b946028df
Prahith <babluprahith@gmmail.com>
2026-02-28 10:29:56 +0530

    feat: New Pages developed

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import CtaSection from '@/components/sections/CtaSection'
+import Plan from '@/components/sections/Plan'
+import Features from '@/components/sections/Features'
 
 const Pricing = () => {
   return (
-    <div>
-      <h1>Pricing</h1>
-    </div>
+    <main>
+      <section className='container-custom'><Header/></section>
+      <Plan/>
+      <Features/>
+      <CtaSection heading="Join the Shift Toward"
+        highlight="Intelligent Ops"
+        description="Whether you're scaling across geographies or just starting your AI journey, Zapverse helps you move faster, with less guesswork."
+        buttonText="Talk to Our Team"/>
+      <Footer/>
+    </main>
   )
 }
 
--- src/pages/WhitePapers.jsx
+++ src/pages/WhitePapers.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import CtaSection from '@/components/sections/CtaSection'
+import Insights from '@/components/sections/Insights'
 
 const WhitePapers = () => {
   return (
-    <div>
-      <h1>WhitePapers</h1>
-    </div>
+    <main>
+      <section className='container-custom'><Header/></section>
+      <Insights/>
+      <CtaSection heading="Join the Shift Toward"
+        highlight="Intelligent Ops"
+        description="Whether you're scaling across geographies or just starting your AI journey, Zapverse helps you move faster, with less guesswork."
+        buttonText="Talk to Our Team"/>
+      <Footer/>
+    </main>
   )
 }
 
